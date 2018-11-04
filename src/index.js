f0e80b2c52c9128aebc8e93cc6a95f9a02627d5d
Khoi Nguyen <khoi.ntq@gmail.com>
2018-11-03 23:38:54 -0400

    Introduction to Redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { configStore } from './app/store/configStore';
+
+const store = configStore();
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 };
